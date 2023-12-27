ff7e17cba1765f9afccd769c4c483aa37c27bc6c
Aman Shah <aman.j.shah@gmail.com>
2023-12-27 15:29:06 +0000

    Student JPA implementation

--- src/main/java/com/practise/gradesubmission/service/StudentServiceImpl.java
+++ src/main/java/com/practise/gradesubmission/service/StudentServiceImpl.java
@@ -2,27 +2,35 @@ package com.practise.gradesubmission.service;
 
 import java.util.Collection;
 import com.practise.gradesubmission.entity.Student;
-import java.util.Collections;
+import com.practise.gradesubmission.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+//This is what needs to be autowired, so @Service annotation goes here rather than on the base interface
+@Service
 public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
 
     @Override
     public Student getStudent(Long id) {
-        return null;
+        return studentRepository.findById(id).get();
     }
 
     @Override
     public Student saveStudent(Student student) {
-        return null;
+        return studentRepository.save(student);
     }
 
     @Override
-    public void deleteStudent(Long id) {        
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
     }
 
     @Override
     public Collection<Student> getStudents() {
-        return Collections.emptyList();
+        return (Collection<Student>) studentRepository.findAll();
     }
 
 
--- src/main/java/com/practise/gradesubmission/web/StudentController.java
+++ src/main/java/com/practise/gradesubmission/web/StudentController.java
@@ -3,6 +3,8 @@ package com.practise.gradesubmission.web;
 import java.util.Collection;
 
 import com.practise.gradesubmission.entity.Student;
+import com.practise.gradesubmission.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,23 +12,28 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
     @GetMapping("/{id}")
     public ResponseEntity<Student> getStudent(@PathVariable Long id){
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(studentService.getStudent(id), HttpStatus.OK);
     }
 
     @GetMapping("/all")
     public ResponseEntity<Collection<Student>> getStudents(){
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(studentService.getStudents(), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Student> saveStudent(@RequestBody Student student){
-        return new ResponseEntity<>(student, HttpStatus.CREATED);
+        return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> deleteStudent(@PathVariable Long id){
+        studentService.deleteStudent(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
