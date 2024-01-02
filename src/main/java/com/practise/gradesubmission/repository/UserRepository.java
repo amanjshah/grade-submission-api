a546ac78f8365f3d172b521d78578682c1652ab9
Aman Shah <aman.j.shah@gmail.com>
2024-01-02 19:09:26 +0000

    add method to get user by username

--- src/main/java/com/practise/gradesubmission/repository/UserRepository.java
+++ src/main/java/com/practise/gradesubmission/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.practise.gradesubmission.repository;
 
+import java.util.Optional;
+
 import com.practise.gradesubmission.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface UserRepository extends CrudRepository<User, Long> {
-	User findByUsername(String username);
+	Optional<User> findByUsername(String username);
 }
--- src/main/java/com/practise/gradesubmission/service/UserService.java
+++ src/main/java/com/practise/gradesubmission/service/UserService.java
@@ -4,5 +4,8 @@ import com.practise.gradesubmission.entity.User;
 
 public interface UserService {
     User getUser(Long id);
+
+    User getUser(String username);
+
     User saveUser(User user);
 }
